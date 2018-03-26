22ec7efde77dace2b8924e3b3ed9520664224338
Cory West-Forbes and Matt Miller <matt@>
2018-03-26 09:29:53 -0700

    Add variables for band, book, instrument. Create array of favorite things and array reordered array. Display in HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,42 @@
 $(function() {
-  var favoriteThingsArray = [];
+  $("#favorite-things-form").submit(function(event) {
 
-  var movieInput = $("#movie-input").val();
+    var favoriteThingsArray = [];
+
+    var movieInput = $("#movie-input").val();
+    var bandInput = $("#band-input").val();
+    var bookInput = $("#book-input").val();
+    var instrumentInput = $("#instrument-input").val();
+
+    // console.log(movieInput, bandInput, bookInput, instrumentInput);
+
+    function addFavoriteThings() {
+      favoriteThingsArray.push(movieInput, bandInput, bookInput, instrumentInput);
+
+      console.log(favoriteThingsArray);
+
+      return favoriteThingsArray;
+    }
+
+    addFavoriteThings();
+
+    function addToNewArray() {
+      var newArray = [];
+      var secondFavoriteItem = favoriteThingsArray[1];
+      var firstFavoriteItem = favoriteThingsArray[0];
+      var thirdFavoriteItem = favoriteThingsArray[2];
+
+      newArray.push(secondFavoriteItem, firstFavoriteItem, thirdFavoriteItem);
+
+      console.log(newArray);
+
+      return newArray;
+    }
+
+    addToNewArray();
+
+    $("#favorite-things-list").append("<li>" + favoriteThingsArray[0] + "</li>", "<li>" + favoriteThingsArray[1] + "</li>", "<li>" + favoriteThingsArray[2] + "</li>");
+
+    event.preventDefault();
+  });
 });
